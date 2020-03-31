8e25589a82e1690a27b12b93e6b255fb25110415
OrlandoTele96 <orlandoglezmusic@gmail.com>
2020-03-30 23:46:58 -0600

    Changes : Node.hpp, Node.cpp

--- Node.cpp
+++ Node.cpp
@@ -3,7 +3,7 @@
 ------------------------------------------------------------------------------*/
 #include "Node.hpp"
 /*Constructors*/
-Node::Node(uint8_t id, uint8_t mtype): ID(id), messagetype(mtype)
+Node::Node(uint8_t id): ID(id)
 {
   //Exceptions
 }
@@ -11,25 +11,16 @@ Node::Node(uint8_t id, uint8_t mtype): ID(id), messagetype(mtype)
 Node::Node(const Node &nodo)
 {
   this->ID=nodo.ID;
-  this->messagetype=nodo.messagetype;
 }
 /*Getters*/
 uint8_t Node::GetID()const
 {
   return this->ID;
 }
-uint8_t Node::GetMessageType()const
-{
-  return this->messagetype;
-}
 /*Setters*/
 void Node::SetID(uint8_t id)
 {
   this->ID=id;
-}
-void Node::SetMessageType(uint8_t mtype)
-{
-  this->messagetype=mtype;
 }
 /*Overt*/
 
--- Node.hpp
+++ Node.hpp
@@ -21,18 +21,15 @@ class Node
 {
 private:
   uint8_t ID;
-  uint8_t messagetype=0x00;
 public:
   /*Constructors*/
   Node()=default;
-  Node(uint8_t id, uint8_t mtype);
+  Node(uint8_t id);
   Node(const Node &nodo);
   /*Getters*/
   uint8_t GetID()const;
-  uint8_t GetMessageType()const;
   /*Setters*/
   void SetID(uint8_t id);
-  void SetMessageType(uint8_t mtype);
   /*Overt*/
 
   /*Others*/
