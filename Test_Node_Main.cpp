1ca932cf6d54c02a8cc0c367b62ee41e86a5a259
OrlandoTele96 <orlandoglezmusic@gmail.com>
2020-03-30 23:52:50 -0600

    First class node is tested.

--- Test_Node_Main.cpp
+++ Test_Node_Main.cpp
@@ -5,24 +5,12 @@
 */
 
 #include "Node.hpp"
-#include <iostream>
 #include <string>
 int main()
 {
-  uint8_t ID=0x2;
-  uint8_t m=0x1;
-  uint16_t message;
-  message=(ID<<8)+m;
-  std::string *i;
-  uint8_t r;
-  Node n1,n2,n3;
-  Node n4(ID,m);
-  ID=0x02;
-  n2.SetID(ID);
-  n3.SetMessageType(m);
-  r=n2.GetID();
-  i=(std::string *)message;
-  cout<<i<<endl;
-
+  Node n;
+  uint8_t id=0x10;
+  n.SetID(id);
+  cout<<(std::string *)n.GetID()<<endl;
   return 0;
 }
