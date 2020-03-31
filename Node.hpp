f8f7c7143a0105f5f4dc8cbf95f0c085dd63431c
OrlandoTele96 <orlandoglezmusic@gmail.com>
2020-03-30 20:53:23 -0600

    new file:   Node.cpp 	new file:   Node.hpp 	new file:   Test_Node_Main.cpp

---
+++ Node.hpp
@@ -0,0 +1,23 @@
+/*------------------------------------------------------------------------------
+--------------------------Class node definition---------------------------------
+------------------------------------------------------------------------------*/
+
+/*
+
+*/
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Node
+{
+private:
+  uint8_t ID;
+  uint8_t messagetype=0x00;
+public:
+  Node()=default;
+  //Node();
+};
