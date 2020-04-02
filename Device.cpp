1d37271eb40dea8a509866b1c69d41b0dbfb5be9
OrlandoTele96 <orlandoglezmusic@gmail.com>
2020-04-01 22:50:40 -0600

    device already pack in a 32 bits variable.

--- Device.cpp
+++ Device.cpp
@@ -3,3 +3,38 @@
 ------------------------------------------------------------------------------*/
 
 #include "Device.hpp"
+/*Constructors*/
+Device::Device(uint8_t mtype,uint16_t m):message_t(mtype),message(m)
+{
+  //
+}
+
+Device::Device(const Device & device):Node(device)
+{
+  this->message_t=device.message_t;
+  this->message=device.message;
+}
+/*Getters*/
+uint8_t Device::getMessageType()const
+{
+  return this->message_t;
+}
+uint16_t Device::getMessage()const{
+  return this->message;
+}
+/*Setters*/
+void Device::setMessageType(uint8_t mt){
+  this->message_t=mt;
+}
+void Device::setMessage(uint16_t m){
+  this->message=m;
+}
+/*Overt*/
+void Device::Pack()
+{
+  if(this->message_t==0x00)
+  {
+    this->message=(this->message_t<<8)+GetID();
+  }
+}
+/*Others*/
--- Device.hpp
+++ Device.hpp
@@ -7,13 +7,28 @@
 #include "Node.hpp"
 #include <iostream>
 
+using namespace std;
 class Device: public Node
 {
 private:
   uint8_t message_t=0x00;
-  uint16_t message;
+  uint32_t message;
 public:
+  /*Constructors*/
   Device()=default;
+  //Device(const Node &nodo);
+  Device(uint8_t mtype,uint16_t m);
+  Device(const Device &device);
+  /*Getters*/
+  uint8_t getMessageType()const;
+  uint16_t getMessage()const;
+  /*Setters*/
+  void setMessageType(uint8_t mt);
+  void setMessage(uint16_t m);
+  /*Overt*/
+  void Pack();
+  /*Others*/
+
 };
 
 #endif
