8fc0079960d9b274cb1dfc2a92ce7bb46e9f7bbd
OrlandoTele96 <orlandoglezmusic@gmail.com>
2020-04-06 01:30:00 -0500

    Changes : LoRa_device/LoRa_device.ino

--- LoRa_device/LoRa_device.ino
+++ LoRa_device/LoRa_device.ino
@@ -1,36 +1,41 @@
-
 #include "heltec.h"
 #include "Node.hpp"
 
 #define BAND 433E6
 
-int interval=2000;
-long lastSendTime=0;
+uint8_t ID=0x01;
+uint8_t C=0x0;
+uint8_t tm=0x00;
 uint32_t message;
 
-Node this_node;
+Node node;
 
 void setup() {
-  // put your setup code here, to run once:
+  //LoRa init
   Heltec.begin(true,true,true,true,BAND);
   LoRa.setSpreadingFactor(7);
   LoRa.onReceive(onReceive);
   LoRa.receive();
+  //Node init
+  node.setID(ID);
   
-  this_node.setID(0x03);
-  this_node.Pack();
 }
 
+
 void loop() {
-  // put your main code here, to run repeatedly:
-  if(millis()-lastSendTime>interval)
-  {
-    message=this_node.getMessage();
-    sendMessage(message);
-    lastSendTime=millis();
-    interval=random(2000)+1000;
-    LoRa.receive();
+
+  while (C<=0x10){
+    //Empaqueta
+    node.setPacket_Number(C);
+    node.setMessage_Type(tm);
+    node.Pack();
+    message=node.getMessage();
+    //Envia
+    
+    //Recibe
   }
+
+  c=0x00;
   
 }
 
@@ -38,22 +43,30 @@ void sendMessage(uint32_t outgoing)
 {
   LoRa.beginPacket();
   LoRa.write(outgoing);
-  LoRa.print(outgoing);  
+  LoRa.print(outgoing);
   LoRa.endPacket();
+
+  C++;
 }
 
 void onReceive(int packetsize)
 {
-  if(packetsize==0) 
-    {
-      return;
-    }
-    else{
-      uint32_t incoming = LoRa.read();
-      this_node.Unpack();
-      Serial.println("Message received");
-    }
-  
-  
+  uint8_t pac_n;
+  if(packetsize==0)
+  {
+    return;
+  }
+  else{
+    uint32_t incoming = LoRa.read();
+    pac_n=this_node.Unpack();
+    Serial.println("Message received");
+  }
+  if(pac_n>=10)
+  {
+    node.Discard();
+    node.GenerateDocument();
+    node.Clear_List();
+  }
   
 }
+
