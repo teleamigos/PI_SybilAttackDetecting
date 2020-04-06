6967da3d1058e061be113ee894d758f5d07f9923
OrlandoTele96 <orlandoglezmusic@gmail.com>
2020-04-06 01:28:48 -0500

    Add a counter of packet number in the payload.

--- Node.cpp
+++ Node.cpp
@@ -3,12 +3,12 @@
 ------------------------------------------------------------------------------*/
 #include "Node.hpp"
 /*Constructors*/
-Node::Node(uint8_t id):ID(id)
+Node::Node(uint8_t id,uint8_t packet_n):ID(id),packet_number(packet_n)
 {
         //Exceptions
 }
-Node::Node(uint8_t id,uint8_t messaget, uint32_t msj):ID(id),message_type(messaget),
-      message(msj)
+Node::Node(uint8_t id,uint8_t messaget,uint8_t packet_n ,uint32_t msj):ID(id),message_type(messaget),
+      packet_number(packet_n),message(msj)
 {
           //Exceptions
 }
@@ -17,6 +17,7 @@ Node::Node(const Node &nodo)
 {
   this->ID=nodo.ID;
   this->message_type=nodo.message_type;
+  this->packet_number=nodo.packet_number;
   this->message=nodo.message;
 }
 
@@ -29,18 +30,34 @@ uint8_t Node::getMessate_Type()const
 {
     return this->message_type;
 }
+uint8_t Node::getPacket_Number()const
+{
+  return this->packet_number;
+}
 uint32_t Node::getMessage()
 {
   return this->message;
+}
+vector<uint8_t> Node::getNeighboors()const
+{
+  return this->Neighboors;
+}
+vector<float> Node::getRSSI_Neighboors()const
+{
+  return this->RSSI_Neighboors;
 }
 /*Setters*/
 void Node::setID(uint8_t id)
 {
   this->ID=id;
 }
-void Node::Messate_Type(uint8_t messaget)
+void Node::setMessage_Type(uint8_t messaget)
 {
   this->message_type=messaget;
+}
+void Node::setPacket_Number(uint8_t packet_n)
+{
+  this->packet_number=packet_n;
 }
 void Node::Message(uint32_t msj)
 {
@@ -49,11 +66,31 @@ void Node::Message(uint32_t msj)
 /*Methods*/
 void Node::Pack()
 {
-  if(this->message_type==0x00)
+  if(this->message_type==0x2)
   {
-    this->message=(this->message_type<<8)+this->ID;
+    this->message=(this->message_type<<24)+(this->packet_number<<16)+(this->ID<<8);
   }
 }
-//void Unpack();
-//void Discard();
-//void GenerateDocument();
+uint8_t Node::Unpack(uint32_t received,float RSSI)
+{
+    uint8_t type = (received >> 24);
+    uint8_t id,c;
+    if(type ==0x2)
+    {
+       c=(received>>16);
+       id=(received>>8);
+       this->Neighboors.push_back(id);
+       this->RSSI_Neighboors.push_back(RSSI);
+    }
+    retunr c;
+}
+void Node::Discard()
+{
+  //In order to discard fake nodes, this method compare a RSSI received with
+  // a RSSI standar for many distances.
+
+}
+void Node::GenerateDocument()
+{
+  //
+}
--- Node.hpp
+++ Node.hpp
@@ -4,33 +4,42 @@
 #ifndef Node_hpp
 #define Node_hpp
 #include <iostream>
+#include <vector>
 using namespace std;
 class Node
 {
 private:
   uint8_t ID;
   uint8_t message_type=0x00;
+  uint8_t packet_number;
   uint32_t message=0x00;
-  //vector<uint8_t> Neighboors;
-  //vector<uint8_t> Fake_nodes;
+  vector<uint8_t> Neighboors;
+  vector<float> RSSI_Neighboors;
+  vector<uint8_t> Fake_nodes;
 public:
   /*Constructors*/
   Node()=default;
-  Node(uint8_t id);
-  Node(uint8_t id,uint8_t messaget,uint32_t msj);
+  Node(uint8_t id,uint8_t packet_n);
+  Node(uint8_t id,uint8_t messaget,uint8_t packet_n,uint32_t msj);
   Node(const Node &nodo);
   /*Getters*/
   uint8_t getID()const;
   uint8_t getMessate_Type()const;
+  uint8_t getPacket_Number()const;
   uint32_t getMessage();
+  vector<uint8_t> getNeighboors()const;
+  vector<float> getRSSI_Neighboors()const;
+  vector<float> getFake_Nodes()const;
   /*Setters*/
   void setID(uint8_t id);
-  void Messate_Type(uint8_t messaget);
+  void setMessage_Type(uint8_t messaget);
+  void setPacket_Number(uint8_t packet_n);
   void Message(uint32_t msj);
   /*Methods*/
   void Pack();
-  void Unpack();
+  uint8_t Unpack(uint32_t received,float RSSI);
   void Discard();
   void GenerateDocument();
+  void Clear_List();
 };
 #endif
