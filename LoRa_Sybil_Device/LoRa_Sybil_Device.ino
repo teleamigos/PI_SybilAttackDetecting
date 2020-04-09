91f70d8ec8ce5b61286f5fa9e24227847f8fa40d
OrlandoTele96 <orlandoglezmusic@gmail.com>
2020-04-08 21:33:50 -0500

    changes : LoRa_Sybil_Device/LoRa_Sybil_Device.ino

--- LoRa_Sybil_Device/LoRa_Sybil_Device.ino
+++ LoRa_Sybil_Device/LoRa_Sybil_Device.ino
@@ -1,66 +1,80 @@
 #include "heltec.h"
 #include "Node.hpp"
 #include "Sybil.hpp"
+#define BAND    433E6  //you can set band here directly,e.g. 868E6,915E6
 
-#define BAND 433E6
-
-uint8_t C=0x0;
-uint8_t id1,id2;
-int n;
-uint32_t message;
-//vector<Node> sybil_nodes;
-
-//Node fake1,fake2;
+  
+byte type_message=0x0;      
+char id='3';       
+char id2='2';      
+byte msgCount = 0;            
+long lastSendTime = 0;        
+int interval = 2000;
+vector<Node> sybil;
 Sybil sybil_nodes;
+int tam;
 
-void setup() {
-  // put your setup code here, to run once:
-  Heltec.begin(true,true,true,true,BAND);
-  LoRa.setSpreadingFactor(7);
+Node n,n2;
+          
+void setup()
+{
+  
+  Heltec.begin(true , true , true , true, BAND );
   LoRa.onReceive(onReceive);
   LoRa.receive();
-  id1=0x02;
-  id2=0x03;
-  sybil_nodes.AddSybilNodes(id1,C);
-  sybil_nodes.AddSybilNodes(id2,C);
+  Serial.println("Heltec.LoRa init succeeded.");
+  n.setID(id);
+  n2.setID(id2);
+  sybil.push_back(n);
+  sybil.push_back(n2);
+  tam=sybil.size();
+  sybil_nodes.setSybilNodes(sybil);
+  sybil_nodes.setN(tam);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  n=sybil_nodes.getN();
-  int i;
-  while(C<=0x10)
+void loop()
+{
+  Node sender;
+  int i=0;
+  if (millis() - lastSendTime > interval)
   {
-    for(i=0;i<n;i++)
+    for(i=0;i<sybil_nodes.getN();i++)
     {
-      message=sybil_nodes.S_Pack(i,C);
-      sendMessage(message);
+      sender=sybil_nodes.S_Pack(i,msgCount);
+      sendMessage(sender);
     }
-    //LoRa.receive();
+    
+    Serial.println("Sending ");
+    Serial.println(id);
+    lastSendTime = millis();            
+    interval = random(2000) + 1000;     
+    LoRa.receive();                     
   }
-  C=0x00;
-
-}
-void sendMessage(uint32_t outgoing)
-{
-  LoRa.beginPacket();
-  LoRa.write(outgoing);
-  LoRa.print(outgoing);
-  LoRa.endPacket();
-
-  C++;
 }
 
-void onReceive(int packetsize)
+void sendMessage(Node sender)
 {
-  uint8_t pac_n;
-  if(packetsize==0)
-  {
-    return;
-  }
-  else{
-    uint32_t incoming = LoRa.read();
-    //pac_n=this_node.Unpack();
-    Serial.println("Message received");
-  }
+  LoRa.beginPacket();                   // start packet
+  LoRa.write(sender.getMessage_Type());
+  LoRa.write(sender.getPacket_Number());
+  LoRa.print(sender.getID());                 
+  LoRa.endPacket();                     
+  msgCount++;                           
+}
+
+void onReceive(int packetSize)
+{
+  if (packetSize == 0) return;          // if there's no packet, return
+           
+  byte tm = LoRa.read();     
+  byte counter = LoRa.read();    
+
+  //String incoming = "";                 
+  char incoming;
+
+  incoming =LoRa.read();      
+  Serial.println("RSSI: " + String(LoRa.packetRssi()));
+  Serial.println("Snr: " + String(LoRa.packetSnr()));
+
+  //Serial.println();
 }
