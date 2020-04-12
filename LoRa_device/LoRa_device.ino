9fc4c6bbe6318a1c17a0ba785019a62345452a6a
Jorge Orlando González Guzmán <43627662+OrlandoTele96@users.noreply.github.com>
2020-04-11 19:16:17 -0500

    Receive rssi and ID : LoRa_device/LoRa_device.ino

--- LoRa_device/LoRa_device.ino
+++ LoRa_device/LoRa_device.ino
@@ -7,7 +7,11 @@ byte type_message=0x0;
 char id='1';             
 byte msgCount = 0;            
 long lastSendTime = 0;        
-int interval = 2000;
+int interval = 4000;
+char ID[10];
+int rssi[10];
+uint8_t type[10];
+int i;
 
 Node n;
           
@@ -23,15 +27,23 @@ void setup()
 
 void loop()
 {
-  
-  
+  int j;
+  if (i>=10)
+  {
+    for(j=0;j<10;j++)
+    {
+      //n.Unpack(type[j],msgCount,ID[j],rssi[j]);
+      Serial.println(rssi[j]);
+    }
+    Serial.println("unpacked succesful");
+    i=0;
+  }
   if (millis() - lastSendTime > interval)
   {
     n.setMessage_Type(type_message);
-    n.setPacket_Nuber(msgCount);
+    n.setPacket_Number(msgCount);
     sendMessage(n);
-    Serial.println("Sending ");
-    Serial.println(id);
+
     lastSendTime = millis();            
     interval = random(2000) + 1000;     
     LoRa.receive();                     
@@ -41,39 +53,30 @@ void loop()
 void sendMessage(Node sender)
 {
   LoRa.beginPacket();                   // start packet
-  LoRa.write(sender.getMessage_Type());
+  LoRa.write(sender.getMessate_Type());
   LoRa.write(sender.getPacket_Number());
   LoRa.print(sender.getID());                 
-  LoRa.endPacket();                     
+  LoRa.endPacket();
+  Serial.println("Sending ");
+  Serial.println(id);                     
   msgCount++;                           
 }
 
 void onReceive(int packetSize)
 {
-  if (packetSize == 0) return;          // if there's no packet, return
-           
-  byte tm = LoRa.read();     
-  byte counter = LoRa.read();    
-
-  //String incoming = "";                 
+ 
+  if (packetSize == 0) return;          // if there's no packet, return        
+  uint8_t tm = LoRa.read();    
+  uint8_t counter = LoRa.read();                     
   char incoming;
-
-  incoming =LoRa.read();      
-
-  n.Unpack(tm,counter,incoming,LoRa.packetRssi());
-  
-
+  incoming =LoRa.read();     
+  int r=LoRa.packetRssi();
+  rssi[i]=r;
   Serial.println("hello received!");
   Serial.println(incoming);
   Serial.println("RSSI: " + String(LoRa.packetRssi()));
-  Serial.println("Snr: " + String(LoRa.packetSnr()));
-
-  if (counter==10)
-  {
-    Serial.println("discard...");
-    n.Discard();
-    n.GenerateDocument();
-    n.Clear_List();
-  }
-  //Serial.println();
+  //n.Unpack(tm,counter,incoming,r);
+  ID[i]=incoming;
+  n.setID(ID[i]);
+  i++;
 }
