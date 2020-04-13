594398a8861e8d536007fe5679fb7d28f5aba3f9
Jorge Orlando González Guzmán <43627662+OrlandoTele96@users.noreply.github.com>
2020-04-13 10:00:32 -0500

    it works

--- LoRa_Sybil_Device/Node.cpp
+++ LoRa_Sybil_Device/Node.cpp
@@ -3,12 +3,12 @@
 ------------------------------------------------------------------------------*/
 #include "Node.hpp"
 /*Constructors*/
-Node::Node(uint8_t id,uint8_t packet_n):ID(id),packet_number(packet_n)
+Node::Node(char id,uint8_t packet_n):ID(id),packet_number(packet_n)
 {
         //Exceptions
 }
-Node::Node(uint8_t id,uint8_t messaget,uint8_t packet_n ,uint32_t msj):ID(id),message_type(messaget),
-      packet_number(packet_n),message(msj)
+Node::Node(char id,uint8_t type_message,uint8_t packet_n):ID(id),message_type(type_message),
+      packet_number(packet_n)
 {
           //Exceptions
 }
@@ -18,15 +18,14 @@ Node::Node(const Node &nodo)
   this->ID=nodo.ID;
   this->message_type=nodo.message_type;
   this->packet_number=nodo.packet_number;
-  this->message=nodo.message;
 }
 
 /*Getters*/
-uint8_t Node::getID()const
+char Node::getID()const
 {
   return this->ID;
 }
-uint8_t Node::getMessate_Type()const
+uint8_t Node::getMessage_Type()const
 {
     return this->message_type;
 }
@@ -34,19 +33,15 @@ uint8_t Node::getPacket_Number()const
 {
   return this->packet_number;
 }
-uint32_t Node::getMessage()
-{
-  return this->message;
-}
-vector<uint8_t> Node::getNeighboors()const
+vector<char> Node::getNeighboors()const
 {
   return this->Neighboors;
 }
-vector<float> Node::getRSSI_Neighboors()const
+vector<int> Node::getRSSI_Neighboors()const
 {
   return this->RSSI_Neighboors;
 }
-vector<uint8_t> Node::getFake_Nodes()const
+vector<char> Node::getFake_Nodes()const
 {
   return this->Fake_nodes;
 }
@@ -55,7 +50,7 @@ vector<float> Node::getRange_Tol()const
   return this->range_tol;
 }
 /*Setters*/
-void Node::setID(uint8_t id)
+void Node::setID(char id)
 {
   this->ID=id;
 }
@@ -67,46 +62,23 @@ void Node::setPacket_Number(uint8_t packet_n)
 {
   this->packet_number=packet_n;
 }
-void Node::Message(uint32_t msj)
-{
-  this->message=msj;
-}
 void Node::setRange_Tol(vector<float>  new_range)
 {
   //
 }
 /*Methods*/
-void Node::Pack()
+void Node::Unpack(char id,int RSSI)
 {
-  if(this->message_type==0x0)
-  {
-    this->message=(this->message_type<<24)+(this->packet_number<<16)+(this->ID<<8);
-  }
-}
-uint8_t Node::Unpack(uint32_t received,float RSSI)
-{
-    uint8_t type = (received >> 24);
-    uint8_t id,c;
-    if(type ==0x0)
-    {
-       c=(received>>16);
-       id=(received>>8);
        this->Neighboors.push_back(id);
        this->RSSI_Neighboors.push_back(RSSI);
-    }
-    return c;
 }
-void Node::Discard()
-{
-  //In order to discard fake nodes, this method compare a RSSI received with
-  // a RSSI standar for many distances.
 
+vector<char> Node::makeList()
+{
   int i,j;
-  vector<uint8_t> id_list;
-  vector<float> rssi_prom;
   bool aux;
   size_t tam;
-  float rssi_aux,c,r_error;
+  vector<char> id_list;
   for (i=0;i<this->Neighboors.size();i++)
   {
        /*Resume ID list*/
@@ -134,6 +106,18 @@ void Node::Discard()
            }
        }
   }
+  return id_list;
+}
+bool Node::Discard(vector<char> id_list)
+{
+  //In order to discard fake nodes, this method compare a RSSI received with
+  // a RSSI standar for many distances.
+
+  int i,j;
+  vector<int> rssi_prom;
+  int rssi_aux,r_error;
+  float c;
+  int a,b;
   for(i=0;i<id_list.size();i++)
   {
       rssi_aux=0;
@@ -148,13 +132,22 @@ void Node::Discard()
       }
       rssi_prom.push_back(rssi_aux/c);
   }
-  r_error=rssi_prom.at(0)-rssi_prom.at(1);
+  //r_error=1-1.5;
+  if(rssi_prom.size()>1)
+  {
+      r_error=rssi_prom.at(0)-rssi_prom.at(1);
 //define distance
 //GetDistance
   if (r_error>this->range_tol.at(0))
   {
        this->Fake_nodes=id_list;
+       return true;
   }
+  else{
+    return false;
+  }
+  }
+
 }
 void Node::GenerateDocument()
 {
