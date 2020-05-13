340f39afcb31ccd999d241e67e51949ea9b2a55f
OrlandoTele96 <orlandoglezmusic@gmail.com>
2020-05-13 11:39:17 -0500

    changes  : proofOfWork, GenerateTarget, packto_hash

--- PoW.hpp
+++ PoW.hpp
@@ -1,7 +1,12 @@
 #ifndef PoW_hpp
 #define PoW_hpp
+
+#include<string>
+using namespace std;
 class PoW{
 public:
-  virtual void proofOfWork(int difficulty)=0;
+  virtual string GenerateTarget(int difficulty)=0;
+  virtual string packto_hash(char ID,string timestamp,string nonce)=0;
+  virtual string proofOfWork(int difficulty)=0;
 };
 #endif
